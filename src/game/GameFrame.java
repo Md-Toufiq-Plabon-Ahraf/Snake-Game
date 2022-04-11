ac82fa1ec60db1f1ad39cbba164da61e02f6a4da
MD Toufiqur Rahman Plabon <rahman15-14579@diu.edu.bd>
2022-04-12 00:23:06 +0600

    Snake Game

---
+++ src/game/GameFrame.java
@@ -0,0 +1,27 @@
+package game;
+
+import javax.swing.JFrame;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import javax.swing.JFrame;
+import java.util.Random; 
+import javax.swing.JPanel;
+
+ 
+public class GameFrame extends JFrame{
+
+	GameFrame(){
+			
+		this.add(new GamePanel());
+		this.setTitle("Snake");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setResizable(false);
+		this.pack();
+		this.setVisible(true);
+		this.setLocationRelativeTo(null);
+		
+	}
+}
